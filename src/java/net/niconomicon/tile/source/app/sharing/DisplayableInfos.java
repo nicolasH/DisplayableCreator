68efaec03085bc2203c5aaaf305b15cbed53fcc8
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-05-16 21:47:37 +0900

    Added some fields to the displayableInfos class

--- src/java/net/niconomicon/tile/source/app/sharing/DisplayableInfos.java
+++ src/java/net/niconomicon/tile/source/app/sharing/DisplayableInfos.java
@@ -3,15 +3,19 @@
  */
 package net.niconomicon.tile.source.app.sharing;
 
+import java.io.File;
 
 /**
  * @author Nicolas Hoibian
- *
+ * 
  */
-public class DisplayableInfos implements Comparable<DisplayableInfos>{
+public class DisplayableInfos implements Comparable<DisplayableInfos> {
 	String title;
 	String location;
 	boolean shouldShare;
+	long size;// in bytes;
+	long width; // in pixels
+	long height; // in pixels
 
 	public DisplayableInfos(String path, String title) {
 		this.title = title;
@@ -19,7 +23,19 @@ public class DisplayableInfos implements Comparable<DisplayableInfos>{
 		this.shouldShare = true;
 	}
 
+	public class InfoLoader implements Runnable {
+		public void run() {
+			File f = new File(location);
+			size = f.length();
+			// load the displayable infos from the file.
+		}
+	}
+
 	public int compareTo(DisplayableInfos o) {
 		return title.toLowerCase().compareTo(o.title.toLowerCase());
+	}
+
+	public String tooltip() {
+		return "Weight : " + size + " Dimensions : " + width + " * " + height + " pixels";
 	}
 }
