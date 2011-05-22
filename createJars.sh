98745cf5d85ae42e97002ef8e3b15b6e9f230bd7
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-05-22 14:15:41 +0900

    Added some echo to the utility scripts

--- createJars.sh
+++ createJars.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "Cleaning, building and signing the project's jar and jnlp"
 mvn clean;mvn package webstart:jnlp-inline
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ if [ "$1" = "prod" ];then
 	echo "deploying to nearlyfreespeech"
 	scp -r target/jnlp/* nhoibian_displayator@ssh.phx.nearlyfreespeech.net:/home/public/DisplayableCreator/
 else
-	echo "copying to displayatorSite"
+	echo "copying locally to displayatorSite - use 'prod' for remote deploy."
 	cp -r target/jnlp/lib /Users/niko/Sites/displayatorSite/DisplayableCreator/
 fi
