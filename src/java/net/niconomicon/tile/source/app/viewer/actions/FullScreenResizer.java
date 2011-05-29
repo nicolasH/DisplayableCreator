216bd4dbdb4f8519efb2e8e309c243f985fa850e
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-05-29 23:46:20 +0200

    Enabled fullscreen mode that works on a mac too. Will set the preview window to fullscreen on screen 0.

--- src/java/net/niconomicon/tile/source/app/viewer/actions/FullScreenResizer.java
+++ src/java/net/niconomicon/tile/source/app/viewer/actions/FullScreenResizer.java
@@ -3,10 +3,12 @@
  */
 package net.niconomicon.tile.source.app.viewer.actions;
 
-import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -40,28 +42,38 @@ public class FullScreenResizer implements ActionListener {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		Point pos = new Point(0, 0);
 
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gs = ge.getScreenDevices();
+
 		if (fullScreen) {
 			fullScreen = false;
 			icon = IconsLoader.getIconsLoader().ic_wExp_24;
 			pos = framePreFullPos;
 			dim = framePreFullDim;
+
 		} else {
 			fullScreen = true;
 			icon = IconsLoader.getIconsLoader().ic_wCon_24;
 			framePreFullDim = frame.getSize();
 			framePreFullPos = frame.getLocation();
 		}
-		// frame.dispose();
+
+		frame.dispose();
 		// // Can only be changed while not displayable.
 		// // but cause problems in mac os x : window appear bellow the toolbar.
-		// frame.setUndecorated(fullScreen);
-		// frame.pack();
-//		frame.setVisible(false);
+		frame.setUndecorated(fullScreen);
+		frame.pack();
+		frame.setVisible(false);
 		button.setIcon(icon);
-		// frame.setAlwaysOnTop(fullScreen);
 		// Can only affect the window if it is displayable
 		frame.setSize(dim);
 		frame.setLocation(pos);
-//		frame.setVisible(true);
+
+		frame.setVisible(true);
+		if (fullScreen) {
+			gs[0].setFullScreenWindow(frame);
+		} else {
+			gs[0].setFullScreenWindow(null);
+		}
 	}
 }
