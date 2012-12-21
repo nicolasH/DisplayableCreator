a5d6336c5febd3974dc79843af8696f57fd49e59
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2012-12-21 11:59:44 +0100

    Added info to create the certificate. (commited in a plane).

--- createJars.sh
+++ createJars.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Cleaning, building and signing the project's jar and jnlp"
+echo "to regenerate a self-signed keystore :"
+echo "keytool -keystore dispKS -genkey -alias DisplayableCreator -keypass '!R4n!D0m!' -validity 1826 -keysize 1024"
 mvn clean;mvn package webstart:jnlp-inline
